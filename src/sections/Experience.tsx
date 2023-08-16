407f3a3495fb5e44501b7c3e3b79d78deb4aa551
Aditya Singh Rajput <adityasrajput786@gmail.com>
2023-08-15 21:44:19 -0400

    Update Experience.tsx

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -13,25 +13,18 @@ function Experience() {
   }, [selected]);
 
   const expereinces = [
-    // {
-    //   name: "CueTech Systems",
-    //   role: "Junior Software Engineer",
-    //   url: "",
-    //   start: "October 2018",
-    //   end: "July 2021",
-    //   shortDescription: [
-    //     "Supported and maintained existing projects, reviewed bugs, reported, and fixed issues in existing software on Python.",
-    //     "Worked on writing unit tests for Python SDK and Python scripts using the Agile development framework.",
-    //     "Involved in web/application development using Python, HTML5, CSS3, AJAX, JSON, and jQuery.",
-    //     "Developed and tested multiple dashboard features using Python, Java, Bootstrap, CSS, JavaScript, and jQuery.",
-    //     "Generated Python Django forms to record online user data and used PyTest for writing test cases.",
-    //     "Developed views and templates with Django view controllers and template language to create a user-friendly website interface.",
-    //     "Used Git version control and deployed projects to Heroku.",
-    //     "Gained experience with Docker and deployed applications inside software containers.",
-    //     "Involved in the design and development phases of Agile software development",
-    //     "Coordinated with a team of developers and designers while interacting with project managers, staff, and other stakeholders to understand user needs.",
-    //   ],
-    // },
+    {
+      name: "George Mason University",
+      role: "Research Assistant",
+      url: "",
+      start: "July 2023",
+      end: "Present",
+      shortDescription: [
+        "Research Assistant for the Department of Information Science and Technology in Fairfax, VA, under Prof Kamlajeet Sanghera.",
+        "Provide robust support in research activities and data gathering.",
+        "Curated an AI-driven course task to educate students on topics such as machine learning, database, and data analytics.",
+      ],
+    },
     {
       name: "W3 Dev Pvt. Ltd.",
       role: "Web Developer Intern",
@@ -39,9 +32,9 @@ function Experience() {
       start: "January 2019",
       end: "August 2019",
       shortDescription: [
-        "I have extensive experience collaborating with cross-functional teams and have been involved in ideating, designing, and deploying new websites from conception to launch, ensuring seamless integration with complex back-end databases and services.",
-        "Additionally, I have contributed to updating existing websites using my expertise in full-stack development to deliver an exceptional user experience to end-users.",
-        "With my technical proficiency and collaborative approach, I have established a reputation as a full-stack developer who consistently delivers exceptional results on time and on budget.",
+        "Developed and implemented advanced algorithms using Python, Django, and Flask to optimize website performance, resulting in a 20% decrease in page load time.",
+        "Collaborated with UI/UX designers and content creators to create visually appealing and user-friendly websites that increased customer engagement by 15%.",
+        "Integrated complex back-end databases and services into existing, improving data retrieval speed by 30% and enhancing overall user experience.",
       ],
     },
     {
@@ -51,8 +44,9 @@ function Experience() {
       start: "July 2018",
       end: "October 2018",
       shortDescription: [
-        "I have successfully executed critical improvements to enhance user interface and experience by utilizing advanced coding techniques in HTML, CSS, and JavaScript frameworks such as jQuery and React.js.",
-        "Additionally, I have effectively translated UI/UX design frameworks into polished, high-quality visual elements through skillful coding in dynamic, fast-paced environments.",
+        "Developed and implemented innovative web design solutions to enhance user experience, resulting in a 30% increase in website traffic",
+        "Optimized website performance by reducing load time by 25% through advanced coding in HTML, CSS, and JavaScript.",
+        "Collaborated with cross-functional teams to troubleshoot and resolve technical issues, improving overall website functionality by 20%.",
       ],
     },
   ];
